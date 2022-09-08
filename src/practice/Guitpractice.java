19e8429ee6029d0ac8f4133956a4d0320a8238cb
DELL <DELL@DELL>
2022-09-08 23:02:04 +0530

    first commit of project

---
+++ src/practice/Guitpractice.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class Guitpractice {
+
+	public static void main(String[] args) {
+
+         System.out.println("this is sample guitGub project");
+         System.out.println("this is first commit of this project");
+         
+
+	}
+
+}
